e6b2dde4bc924c1490a822b59c9423b2317c847e
Laura Rajala <laura.rajala@myy.haaga-helia.fi>
2018-05-03 18:02:05 +0300

    added git clone saltstack-dev-setup.git

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ set -o verbose
 echo "Installing Salt and Git..."
 sudo apt-get update
 sudo apt-get -y install git salt-minion
+git clone https://github.com/kdechant/saltstack-dev-setup.git
 
 echo "Retrieving and Applying Settings..."
 echo 'master: localhost'|sudo tee /etc/salt/minion
