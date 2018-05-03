477785a81ecbca4f9cd11e7d0c84957180b85b9e
Laura Rajala <laura.rajala@myy.haaga-helia.fi>
2018-05-03 18:11:14 +0300

    deleted the .git from git clone .... address in run.sh

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ sudo apt-get -y install git salt-minion
 
 echo "Retrieving and Applying Settings..."
 echo 'master: localhost'|sudo tee /etc/salt/minion
-git clone https://github.com/laurarajala/terotin.git
+git clone https://github.com/laurarajala/terotin
 cd terotin/
 './highstate.sh'
 
